bedaeb8f31d26e9af6785201efb45b98ec2a06b0
smnm1998 <dltpals7498@gmail.com>
2025-10-29 13:53:13 +0900

    test: Lotto 클래스 검증 로직 테스트 작성
    - 개수, 중복, 타입, 범위 검증 테스트 케이스 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,48 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 로또 번호는 6개여야 합니다!');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR] 중복된 숫자가 있으면 안됩니다!');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('로또 번호에 문자열이 포함되면 예외가 발생한다', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 'abc']);
+    }).toThrow('[ERROR] 로또 번호를 다시 한 번 확인해주세요!');
+  });
+
+  test.each([[0], [-1], [-100]])(
+    '로또 번호가 1보다 작으면 예외 발생: %i',
+    (invalidNumber) => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, invalidNumber]);
+      }).toThrow('[ERROR] 로또 번호는 1~45 사이여야 합니다!');
+    },
+  );
+
+  test.each([[46], [75], [100]])(
+    '로또 번호가 45를 초과하면 예외 발생: %i',
+    (invalidNumber) => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, invalidNumber]);
+      }).toThrow('[ERROR] 로또 번호는 1~45 사이여야 합니다!');
+    },
+  );
+
+  test.each([[null], [undefined]])(
+    '로또 번호에 null 또는 undefined가 포함되면 예외가 발생한다',
+    (invalidNumber) => {
+      expect(() => {
+        new Lotto([1, 2, 3, 4, 5, invalidNumber]);
+      }).toThrow('[ERROR] 로또 번호를 다시 한 번 확인해주세요!');
+    },
+  );
 });
