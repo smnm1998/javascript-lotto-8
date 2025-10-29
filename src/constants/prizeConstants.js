65d52ac6c25499a664efb983e4cd238b1f469a6a
smnm1998 <dltpals7498@gmail.com>
2025-10-29 13:27:53 +0900

    feat: 로또 당첨 상금 관련 상수 정의

---
+++ src/constants/prizeConstants.js
@@ -0,0 +1,7 @@
+export const PRIZE = {
+  FIRST: { match: 6, bonus: false, amount: 2000000000 },
+  SECOND: { match: 5, bonus: true, amount: 30000000 },
+  THIRD: { match: 5, bonus: false, amount: 1500000 },
+  FOURTH: { match: 4, bonus: false, amount: 50000 },
+  FIFTH: { match: 3, bonus: false, amount: 5000 },
+};
