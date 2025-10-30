61a87358d814b278819d0c4360db4cc0b5e5a6c8
smnm1998 <dltpals7498@gmail.com>
2025-10-30 11:46:38 +0900

    feat: WinningNumber 클래스 구현
    - Lotto 객체를 활용한 당첨 번호 관리
    - 보너스 번호 검증 로직 (타입, 범위, 중복)
    - 당첨 번호 및 보너스 번호 조회 메서드 제공

---
+++ src/domain/WinningNumber.js
@@ -0,0 +1,48 @@
+import { LOTTO_CONFIG } from '../constants/lottoConstants.js';
+import Lotto from './Lotto.js';
+
+class WinningNumber {
+  #winningLotto;
+  #bonusNumber;
+
+  constructor(numbers, bonusNumber) {
+    this.#winningLotto = new Lotto(numbers);
+    this.#validateBonusType(bonusNumber);
+    this.#validateBonusRange(bonusNumber);
+    this.#validateBonusDuplicate(numbers, bonusNumber);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  #validateBonusType(bonusNumber) {
+    if (!Number.isInteger(bonusNumber)) {
+      throw new Error('[ERROR] 보너스 번호는 숫자여야 합니다!');
+    }
+  }
+
+  #validateBonusRange(bonusNumber) {
+    if (
+      bonusNumber < LOTTO_CONFIG.MIN_NUMBER ||
+      bonusNumber > LOTTO_CONFIG.MAX_NUMBER
+    ) {
+      throw new Error(
+        '[ERROR] 보너스 번호는 1 이상 45 이하의 수만 가능합니다!',
+      );
+    }
+  }
+
+  #validateBonusDuplicate(numbers, bonusNumber) {
+    if (numbers.includes(bonusNumber)) {
+      throw new Error('[ERROR] 당첨 번호와 보너스 번호는 중복되면 안됩니다!');
+    }
+  }
+
+  getWinningNumbers() {
+    return this.#winningLotto.getNumbers();
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+}
+
+export default WinningNumber;
