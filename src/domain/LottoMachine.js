77bcec93b0fc1bf32229354bb288310bd4033afe
smnm1998 <dltpals7498@gmail.com>
2025-10-31 09:23:39 +0900

    feat: LottoMachine 로또 발행 기능 구현
    - RandomAPI 활용 번호 자동 생성
    - 구매 금액에 따른 로또 수량 계산 및 발행
    - 구매 금액 검증 (1000원 단위, 최소 금액)

---
+++ src/domain/LottoMachine.js
@@ -0,0 +1,39 @@
+import { Random } from '@woowacourse/mission-utils';
+import Lotto from './Lotto';
+import { LOTTO_CONFIG } from '../constants/lottoConstants';
+
+class LottoMachine {
+  createLottos(purchaseAmount) {
+    this.#validatePurchaseAmount(purchaseAmount);
+
+    const count = purchaseAmount / LOTTO_CONFIG.PRICE;
+    const lottos = [];
+
+    for (let i = 0; i < count; i++) {
+      lottos.push(this.#createLotto());
+    }
+
+    return lottos;
+  }
+
+  #validatePurchaseAmount(amount) {
+    if (amount < 0) {
+      throw new Error('[ERROR] 구입 금액은 최소 1000원 부터입니다!');
+    }
+
+    if (amount % LOTTO_CONFIG.PRICE !== 0) {
+      throw new Error('[ERROR] 구입 금액이 1000원 단위가 아닙니다!');
+    }
+  }
+
+  #createLotto() {
+    const numbers = Random.pickUniqueNumbersInRange(
+      LOTTO_CONFIG.MIN_NUMBER,
+      LOTTO_CONFIG.MAX_NUMBER,
+      LOTTO_CONFIG.NUMBER_COUNT,
+    );
+    return new Lotto(numbers);
+  }
+}
+
+export default LottoMachine;
