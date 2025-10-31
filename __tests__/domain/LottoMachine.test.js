c1c0ae84f8059a5a8b5c3ec07341dbb00520e67f
smnm1998 <dltpals7498@gmail.com>
2025-10-31 09:22:45 +0900

    test: LottoMachine 로또 발행 테스트
    - 구매 금액에 따른 로또 개수 생성 테스트
    - 로또 번호 개수 검증 테스트
    - 구매 금액 예외 처리 테스트 (1000원 단위, 최소 금액)

---
+++ __tests__/domain/LottoMachine.test.js
@@ -0,0 +1,37 @@
+import LottoMachine from '../../src/domain/LottoMachine.js';
+
+describe('LottoMachine', () => {
+  test('구매 금액에 맞는 로또 개수를 생성한다.', () => {
+    const machine = new LottoMachine();
+    const lottos = machine.createLottos(5000);
+
+    expect(lottos.length).toBe(5);
+  });
+
+  test('생성된 로또는 6개 번호를 가진다.', () => {
+    const machine = new LottoMachine();
+    const lottos = machine.createLottos(1000);
+
+    lottos.forEach((lotto) => {
+      expect(lotto.getNumbers()).toHaveLength(6);
+    });
+  });
+
+  describe('LottoMachine - 예외 케이스', () => {
+    test('0원 미만이면 예외가 발생한다.', () => {
+      const machine = new LottoMachine();
+
+      expect(() => {
+        machine.createLottos(-1000);
+      }).toThrow('[ERROR] 구입 금액은 최소 1000원 부터입니다!');
+    });
+
+    test('1000원 단위가 아니면 예외가 발생한다.', () => {
+      const machine = new LottoMachine();
+
+      expect(() => {
+        machine.createLottos(1500);
+      }).toThrow('[ERROR] 구입 금액이 1000원 단위가 아닙니다!');
+    });
+  });
+});
