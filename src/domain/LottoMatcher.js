16e789bf3556811afb534e9a6973c3fa7e6ab21f
smnm1998 <dltpals7498@gmail.com>
2025-11-01 16:48:15 +0900

    refactor: 도메인 객체 단순화

--- src/domain/LottoMatcher.js
+++ src/domain/LottoMatcher.js
@@ -1,24 +1,12 @@
 import Rank from './Rank.js';
 
 class LottoMatcher {
-  #lotto;
-  #winningNumber;
   #matchCount;
   #hasBonus;
 
   constructor(lotto, winningNumber) {
-    this.#lotto = lotto;
-    this.#winningNumber = winningNumber;
-    this.#matchCount = this.#calculateMatch();
-    this.#hasBonus = this.#checkBonus();
-  }
-
-  #calculateMatch() {
-    return this.#winningNumber.countMatchesWith(this.#lotto);
-  }
-
-  #checkBonus() {
-    return this.#winningNumber.isBonusMatch(this.#lotto);
+    this.#matchCount = winningNumber.countMatchesWith(lotto);
+    this.#hasBonus = winningNumber.isBonusMatch(lotto);
   }
 
   determineRank() {
